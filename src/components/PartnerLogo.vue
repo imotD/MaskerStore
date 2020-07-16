d4787ef02d52240ac49f34c076566ac951a32eea
Tommy Alhamra <tommy.alhamra@gmail.com>
2020-07-16 17:25:11 +0700

    first commit

---
+++ src/components/PartnerLogo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="partner-logo">
+            <div class="container">
+                <div class="logo-carousel owl-carousel">
+                    <div class="logo-item">
+                        <div class="tablecell-inner">
+                            <img src="img/logo-carousel/logo-1.png" alt="" />
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="tablecell-inner">
+                            <img src="img/logo-carousel/logo-2.png" alt="" />
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="tablecell-inner">
+                            <img src="img/logo-carousel/logo-3.png" alt="" />
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="tablecell-inner">
+                            <img src="img/logo-carousel/logo-4.png" alt="" />
+                        </div>
+                    </div>
+                    <div class="logo-item">
+                        <div class="tablecell-inner">
+                            <img src="img/logo-carousel/logo-5.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PartnerLogo'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="home">
+        <Header />
+        <HeroSlideShow />
+        <WomenStyle />
+        <InstagramPhoto />
+        <PartnerLogo />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import HeroSlideShow from '@/components/HeroSlideShow.vue'
+import WomenStyle from '@/components/WomenStyle.vue'
+import InstagramPhoto from '@/components/InstagramPhoto.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        Header,
+        HeroSlideShow,
+        WomenStyle,
+        InstagramPhoto,
+        PartnerLogo,
+        Footer
+    }
+}
+</script>
