4bd9f284da91565a9cc376c003a0f86ba9673fab
Tommy Alhamra <tommy.alhamra@gmail.com>
2020-07-24 16:06:51 +0700

    Api Product

--- src/components/WomenStyle.vue
+++ src/components/WomenStyle.vue
@@ -3,11 +3,11 @@
         <section class="women-banner spad">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-12 mt-5">
+                    <div class="col-lg-12 mt-5" v-if="products.length > 0">
                         <carousel class="product-slider" :items="3" :nav="false" :autoplay="tre" :dots="false">
-                            <div class="product-item">
+                            <div class="product-item" v-for="itemProduct in products" :key="itemProduct.id">
                                 <div class="pi-pic">
-                                    <img src="img/mickey1.jpg" alt="" />
+                                    <img :src="itemProduct.galleries[0].photo" alt="img" />
                                     <ul>
                                         <li class="w-icon active">
                                             <a href="#"><i class="icon_bag_alt"></i></a>
@@ -20,80 +20,83 @@
                                     </ul>
                                 </div>
                                 <div class="pi-text">
-                                    <div class="catagory-name">Coat</div>
+                                    <div class="catagory-name">{{itemProduct.type}}</div>
                                     <router-link to="/product">
-                                        <h5>Mickey Baggy</h5>
+                                        <h5>{{itemProduct.name}}</h5>
                                     </router-link>
                                     <div class="product-price">
-                                        $14.00
+                                        {{itemProduct.price}}
                                         <span>$35.00</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="product-item">
-                                <div class="pi-pic">
-                                    <img src="img/products/women-2.jpg" alt="" />
-                                    <ul>
-                                        <li class="w-icon active">
-                                            <a href="#"><i class="icon_bag_alt"></i></a>
-                                        </li>
-                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    </ul>
-                                </div>
-                                <div class="pi-text">
-                                    <div class="catagory-name">Shoes</div>
-                                    <a href="#">
-                                        <h5>Guangzhou sweater</h5>
-                                    </a>
-                                    <div class="product-price">
-                                        $13.00
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="product-item">
-                                <div class="pi-pic">
-                                    <img src="img/products/women-3.jpg" alt="" />
-                                    <ul>
-                                        <li class="w-icon active">
-                                            <a href="#"><i class="icon_bag_alt"></i></a>
-                                        </li>
-                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    </ul>
-                                </div>
-                                <div class="pi-text">
-                                    <div class="catagory-name">Towel</div>
-                                    <a href="#">
-                                        <h5>Pure Pineapple</h5>
-                                    </a>
-                                    <div class="product-price">
-                                        $34.00
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="product-item">
-                                <div class="pi-pic">
-                                    <img src="img/products/women-4.jpg" alt="" />
-                                    <ul>
-                                        <li class="w-icon active">
-                                            <a href="#"><i class="icon_bag_alt"></i></a>
-                                        </li>
-                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                        <li class="w-icon">
-                                            <a href="#"><i class="fa fa-random"></i></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="pi-text">
-                                    <div class="catagory-name">Towel</div>
-                                    <a href="#">
-                                        <h5>Converse Shoes</h5>
-                                    </a>
-                                    <div class="product-price">
-                                        $34.00
-                                    </div>
-                                </div>
-                            </div>
+                            <!-- <div class="product-item">
+                                                                        <div class="pi-pic">
+                                                                            <img src="img/products/women-2.jpg" alt="" />
+                                                                            <ul>
+                                                                                <li class="w-icon active">
+                                                                                    <a href="#"><i class="icon_bag_alt"></i></a>
+                                                                                </li>
+                                                                                <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                                                            </ul>
+                                                                        </div>
+                                                                        <div class="pi-text">
+                                                                            <div class="catagory-name">Shoes</div>
+                                                                            <a href="#">
+                                                                                <h5>Guangzhou sweater</h5>
+                                                                            </a>
+                                                                            <div class="product-price">
+                                                                                $13.00
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="product-item">
+                                                                        <div class="pi-pic">
+                                                                            <img src="img/products/women-3.jpg" alt="" />
+                                                                            <ul>
+                                                                                <li class="w-icon active">
+                                                                                    <a href="#"><i class="icon_bag_alt"></i></a>
+                                                                                </li>
+                                                                                <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                                                            </ul>
+                                                                        </div>
+                                                                        <div class="pi-text">
+                                                                            <div class="catagory-name">Towel</div>
+                                                                            <a href="#">
+                                                                                <h5>Pure Pineapple</h5>
+                                                                            </a>
+                                                                            <div class="product-price">
+                                                                                $34.00
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="product-item">
+                                                                        <div class="pi-pic">
+                                                                            <img src="img/products/women-4.jpg" alt="" />
+                                                                            <ul>
+                                                                                <li class="w-icon active">
+                                                                                    <a href="#"><i class="icon_bag_alt"></i></a>
+                                                                                </li>
+                                                                                <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                                                                <li class="w-icon">
+                                                                                    <a href="#"><i class="fa fa-random"></i></a>
+                                                                                </li>
+                                                                            </ul>
+                                                                        </div>
+                                                                        <div class="pi-text">
+                                                                            <div class="catagory-name">Towel</div>
+                                                                            <a href="#">
+                                                                                <h5>Converse Shoes</h5>
+                                                                            </a>
+                                                                            <div class="product-price">
+                                                                                $34.00
+                                                                            </div>
+                                                                        </div>
+                                                                    </div> -->
                         </carousel>
+                    </div>
+                    <div class="col-lg-12" v-else>
+                        <p>Produk tidak tersedia</p>
                     </div>
                 </div>
             </div>
@@ -103,13 +106,26 @@
 
 <script>
 import carousel from 'vue-owl-carousel'
+import axios from 'axios';
 
 
 export default {
     name: 'WomenStyle',
     components: {
         carousel
-    }
+    },
+    data() {
+        return {
+            products: []
+        }
+    },
+    mounted() {
+        axios
+            .get("http://shayna-backend.belajarkoding.com/api/products")
+            .then(res => (this.products = res.data.data.data))
+            .catch(err => console.log(err));
+
+    },
 }
 </script>
 
